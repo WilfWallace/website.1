dec9d115f127001d0b8030c45d4bd9986c52f448
WilfWallace <wilf.wallace2@outlook.com>
2025-05-07 20:13:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ let moreLess = true;
 function buttonclicked() {
   if (moreLess) {
     document.getElementById("Solar more/less").innerHTML = "This project was undertaken initially as coursework during my A-levels and has been updated/developed many times since. The code uses a weather API to create a prediction for how much energy the users solar panels are likely to generate. It uses this information along with the size of the users battery and their daily energy usage to calculate an optimum energy plan that saves the user money.";
-    document.getElementById("buttonMoreLess").innerHTML = "Read Less";
+    document.getElementById("solar m/l button").innerHTML = "Read Less";
     moreLess = false;
   } else {
     document.getElementById("Solar more/less").innerHTML = "";
-    document.getElementById("buttonMoreLess").innerHTML = "Read more";
+    document.getElementById("solar m/l button").innerHTML = "Read more";
     moreLess = true;
   }
 }
