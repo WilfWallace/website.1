f932268057659193fd5bb1e9ab0e6f24bb93af72
WilfWallace <wilf.wallace2@outlook.com>
2025-05-07 20:40:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function solarbuttonclicked() {
     solarmoreless = true;
   }
 }
-let renishawmoreless = true
+let renishawmoreless = true;
 function renishawbuttonclicked() {
   if (renishawmoreless) {
     document.getElementById("renishaw-ml").innerHTML = "During this work experience, I spent a week working in a team of 5 to design and produce a prototype of a green energy product. The product my team design was a speed bumb that converted GPE of the car into KE energy in the motor. My role in this project was to design the prototype using CAD software and 3D print it. At the end of the week, we presented our prototypes to the other teams and some renishaw employees. The main thing I took away from this week was how to work effectively in a team and how to present my ideas and findings to a crowd.";
