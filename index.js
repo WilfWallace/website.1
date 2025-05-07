91a3cfef3f2656d3c9afcb140d2bdb234642270e
WilfWallace <wilf.wallace2@outlook.com>
2025-05-07 20:23:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function solarbuttonclicked() {
   if (solarmoreLess) {
     document.getElementById("Solar more/less").innerHTML = "This project was undertaken initially as coursework during my A-levels and has been updated/developed many times since. The code uses a weather API to create a prediction for how much energy the users solar panels are likely to generate. It uses this information along with the size of the users battery and their daily energy usage to calculate an optimum energy plan that saves the user money.";
     document.getElementById("solar m/l button").innerHTML = "Read Less";
-    moreLess = false;
+    solarmoreLess = false;
   } else {
     document.getElementById("Solar more/less").innerHTML = "";
     document.getElementById("solar m/l button").innerHTML = "Read more";
-    moreLess = true;
+    solarmoreLess = true;
   }
 }
 let renishawmoreless = true
@@ -15,10 +15,10 @@ function renishawbuttonclicked() {
   if (renishawmoreLess) {
     document.getElementById("renishaw m/l").innerHTML = "During this work experience, I spent a week working in a team of 5 to design and produce a prototype of a green energy product. The product my team design was a speed bumb that converted GPE of the car into KE energy in the motor. My role in this project was to design the prototype using CAD software and 3D print it. At the end of the week, we presented our prototypes to the other teams and some renishaw employees. The main thing I took away from this week was how to work effectively in a team and how to present my ideas and findings to a crowd.";
     document.getElementById("renishaw m/l button").innerHTML = "Read Less";
-    moreLess = false;
+    renishawmoreLess = false;
   } else {
     document.getElementById("renishaw m/l").innerHTML = "";
     document.getElementById("renishaw m/l button").innerHTML = "Read more";
-    moreLess = true;
+    renishawmoreLess = true;
   }
 }
